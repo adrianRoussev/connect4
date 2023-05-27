c34838d3e1809004fb44eff8546e5f33529c2597
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-27 10:31:12 -0600

    test: make detailed initialize tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,22 @@
-require '/board'
+require './board'
 require './lib/game'
 
 RSpec.describe Game do 
   describe "initialize" do 
     it "exists" do 
+      game = Game.new
+      expect(game).to be_an_instance_of(Game)
     end 
 
     it "can create new board instances" do 
+      game = Game.new
+      expect(game.board).to be_an_instance_of(Board)
     end  
 
-    it "can print a new game message" do 
+    it "can print a new game message without repeating" do 
+      game = Game.new
+      expect(game.greeting).to eq("Welcome to Connect 4!")
+      expect(game.greeting).to eq(nil)
     end 
   end 
 end 
