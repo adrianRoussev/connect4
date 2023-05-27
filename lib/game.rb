a7998828bbfca02d52d7d2cf08b7640619fde1de
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-27 13:19:50 -0600

    feat: add the print board method to print empty board

--- lib/game.rb
+++ lib/game.rb
@@ -11,4 +11,17 @@ class Game
     def greeting 
       @start ? (@start = false; "Welcome to Connect 4!") : nil
     end  
+
+    def print_board
+      output = ""
+      output += ("A".."G").to_a.join(" ") + "\n"
+  
+      6.times do
+        row_output = ["."] * @board.columns
+        output += row_output.join(" ") + "\n"
+      end
+  
+      output
+    end
+
 end 
