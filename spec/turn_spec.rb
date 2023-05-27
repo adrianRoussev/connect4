18cb4cf9e3b6840441ccbe20c433532b808de26e
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-27 14:47:49 -0600

    test: add test to initialize player as the two symbols we will be using

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,10 +5,17 @@ require './lib/turn'
 
 RSpec.describe Turn do 
   describe "initialize" do 
-    it "exists"
-    turn = Turn.new
+    it "exists" do 
+    turn = Turn.new("turn")
     expect(turn).to be_an_instance_of(Turn)
   end 
-
+  it "can assign turn as either :X or :O" do
+    turn = Turn.new(:X)
+    turn2 = Turn.new(:O)
+    expect(turn.player).to eq(:X)
+    expect(turn2.player).to eq(:O)
+  end 
+  
+end 
 
 end 
