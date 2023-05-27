c870bb389e4099f974d6eaeba749e47d329d709d
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-27 11:26:35 -0600

    test: create test for initial turn value and print board test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,13 +18,43 @@ RSpec.describe Game do
       expect(game.greeting).to eq("Welcome to Connect 4!")
       expect(game.greeting).to eq(nil)
     end 
+    it "has an inital turn value of :X" do 
+      game = Game.new
+      expect(game.turn).to eq(:X)
+    end 
   end 
 
   describe "game flow methods" do 
-    it "can print board" do 
+    xit "can print board" do
+      game = Game.new
+  
+      expected_output = 
+      "A B C D E F G\n" \
+      ".......\n" \
+      ".......\n" \
+      ".......\n" \
+      ".......\n" \
+      ".......\n" \
+      ".......\n"
+
+      rows = expected_output.split("\n")
+  
+      expect(expected_output.length).to eq(48)
+      expect(expected_output[0].split.length).to eq(7)
+      expect(expected_output[1].split.length).to eq(7)
+      expect(rows[0].split.length).to eq(7)
+      expect(rows[1].split.length).to eq(7)
+      # ^ eq(7) to include the A-G column identifiers
+      expect(game.printboard).to eq(expected_output)
+      #use pry to verify the "eye test" as well
     end 
 
-    it "can keep track of turn" do 
-    end 
+    # it "correctly prints placed pieces onto the board" do 
+    # obv need to incorporate the take turn methods before checking this
+    # end 
+
+    # it "can keep track of turns" do 
+    # obv need to incorporate the take turn methods before checking this
+    # end 
   end 
 end 
