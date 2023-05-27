cbd372ba727526a721d0bead02642163ac6440ae
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-27 10:22:08 -0600

    test: create inital game class tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+require '/board'
+require './lib/game'
+
+RSpec.describe Game do 
+  describe "initialize" do 
+    it "exists" do 
+    end 
+
+    it "can create new board instances" do 
+    end  
+
+    it "can print a new game message" do 
+    end 
+  end 
+end 
