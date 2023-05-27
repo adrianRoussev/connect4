cf8f78f4f22d159878fde741e828675dcca96749
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-27 15:11:55 -0600

    test: initial value of column input attritbute

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,16 +6,18 @@ require './lib/turn'
 RSpec.describe Turn do 
   describe "initialize" do 
     it "exists" do 
-    turn = Turn.new("turn")
-    expect(turn).to be_an_instance_of(Turn)
-  end 
-  it "can assign turn as either :X or :O" do
-    turn = Turn.new(:X)
-    turn2 = Turn.new(:O)
-    expect(turn.player).to eq(:X)
-    expect(turn2.player).to eq(:O)
-  end 
-  
-end 
-
-end 
+      turn = Turn.new("turn")
+      expect(turn).to be_an_instance_of(Turn)
+    end 
+    it "can assign turn as either :X or :O" do
+      turn = Turn.new(:X)
+      turn2 = Turn.new(:O)
+      expect(turn.player).to eq(:X)
+      expect(turn2.player).to eq(:O)
+    end 
+    it "has default value of nil for column input" do 
+      turn = Turn.new("turn")
+      expect(turn.column_input).to be nil 
+    end 
+  end
+end  
