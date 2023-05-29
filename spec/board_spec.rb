3e3db2c2444dda1357a5bfcb5891fbbfeb78d095
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-29 11:15:06 -0600

    test: test initial row and column values

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,19 +3,25 @@ require './board'
 
 
 RSpec.describe Board do 
+  before do
+    @board = Board.new
+  end
   describe "initialize" do 
     it "exists" do 
-      @board = Board.new
       expect(@board).to be_an_instance_of(Board)
     end 
 
     it "board and markers are initialized as bits" do
-      @board = Board.new
       expected = 0b1111110111111011111101111111011111101111110111111
 
       expect(@board.marker_positions_bit[:X]).to eq(0b0)
       expect(@board.marker_positions_bit[:O]).to eq(0b0)
       expect(@board.full_board).to eq(expected)
     end
+
+    it "has the correct number of rows and columns" do 
+      expect(@board.rows).to eq(7)
+      expect(@board.columns).to eq (7)
+    end 
   end 
 end 
