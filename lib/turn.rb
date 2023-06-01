57be0ed2012a51a8e93e1da972708382993200c8
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-31 21:38:01 -0400

    add  game_over? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,19 +9,7 @@ class Turn
   end 
 
 
-  def get_move
-    valid_columns = ("A".."G").to_a
-    "#{player}, it's your turn. Enter a column (A-G) to make your move:"
-    input = gets.chomp.upcase
   
-    until input.is_a?(String) && valid_columns.include?(input.upcase)
-      "Invalid column. Please enter a column (A-G):"
-      input = gets.chomp.upcase
-    end
-  
-    column_index = valid_columns.index(input.upcase)
-    @board.make_move(column_index, @player)
-  end
 
   def get_move_with_input(input)
     #This is a method for test purposes, testing gets.chomp was quite difficult.
